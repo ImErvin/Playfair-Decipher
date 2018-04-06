7ff6c9d01f08111f36cb23a1b8065bd630247635
Ervin Mamutov <erviin@live.ie>
2018-04-07 00:40:01 +0100

    Can successfully format a piece of text before digramming

--- src/ie/gmit/sw/Cypher.java
+++ src/ie/gmit/sw/Cypher.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 // Adapted from https://gist.github.com/tahaemara/473702df72163773d8393d93428c4b6b
 public class Cypher {
-	public ArrayList<String> digrams;
+	private ArrayList<String> digrams;
 	
-	public Cypher() {
-		// TODO Auto-generated constructor stub
+	public Cypher(String text) {
+		this.digrams = this.digramify(this.formatify(text));
 	}
 	
 	public ArrayList<String> digramify(String text){
@@ -20,14 +20,34 @@ public class Cypher {
 		return digrams;
 	}
 	
+	public String formatify(String text){
+		String plainText = text.toUpperCase().replaceAll("[^A-Za-z0-9]", "");
+		StringBuffer sb = new StringBuffer(plainText);
+		
+		for(int i = 0; i < plainText.length() - 1; i += 2){
+			if(sb.charAt(i) == sb.charAt(i+1)){
+				sb.insert(i+1, "X");
+			}
+		}
+		if(sb.length() % 2 == 1){
+			sb.append("X");
+		}
+
+		return sb.toString();
+	}
+	
 	public String decrypt(){
 		return null;
 	}
 	
+	public ArrayList<String> getDigrams(){
+		return this.digrams;
+	}
+	
 	
 	public static void main(String[] args) {
-		Cypher c = new Cypher();
+		Cypher c = new Cypher("This is a test message for digramst");
 		
-		System.out.println(c.digramify("This is a test message for digrams"));
+		System.out.println(c.getDigrams());
 	}
 }
