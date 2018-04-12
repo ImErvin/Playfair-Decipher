7a48424984b2df0be7faf0be6b5c18c3024e159a
Ervin Mamutov <erviin@live.ie>
2018-04-12 19:21:53 +0100

    Can parse 4grams and calculate total

--- src/ie/gmit/sw/Scoring/Score.java
+++ src/ie/gmit/sw/Scoring/Score.java
@@ -1,5 +1,51 @@
 package ie.gmit.sw.Scoring;
 
-public class Score {
+import java.io.*;
+import java.util.*;
 
+public class Score {
+	private Map<String, Integer> fourgrams;
+	private long total;
+	private FileReader fr;
+	private BufferedReader br;
+	
+	public Score() {
+		this.fourgrams = parseFile();
+		this.total = calcTotal();
+		System.out.println(this.total);
+	}
+	
+	public Map<String, Integer> parseFile(){
+		Map<String, Integer> fourgrams = new HashMap<String, Integer>();
+		
+		try {
+			String line = null;
+			String[] text = new String[2];
+			this.fr = new FileReader("./4grams.txt");
+			this.br = new BufferedReader(this.fr);
+			
+			while((line = br.readLine()) != null) {
+				text = line.split(" ");
+				fourgrams.put(text[0], Integer.parseInt(text[1]));
+            }   
+			
+			return fourgrams;
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+			return null;
+		} catch (IOException e) {
+			System.out.println("Error reading file");
+			return null;
+		}
+	}
+	
+	// Ripped from https://stackoverflow.com/questions/30089469/how-to-sum-values-in-a-map-with-a-stream
+	public long calcTotal(){
+		return this.fourgrams.values().stream().mapToLong(Number::intValue).sum();
+	}
+	
+	public static void main(String[] args) {
+		Score s = new Score();
+	}
+	
 }
